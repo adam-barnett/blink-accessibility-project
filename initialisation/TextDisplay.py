a8d6eb97102c60451a8f8c1974273559266d449b
adam-barnett <adam.barnett23@gmail.com>
2015-03-10 11:01:46 -0400

    Altered text display to get rid of text timers and allow displaying messages for a fixed time.

--- initialisation/TextDisplay.py
+++ initialisation/TextDisplay.py
@@ -9,7 +9,7 @@ a countdown.
 
 class TextDisplay(wx.Frame):
 
-    def __init__(self, start_up_msg=""):
+    def __init__(self, start_up_msg="", time=0):
         (screen_width, screen_height) = wx.DisplaySize()
         wx.Frame.__init__(self, None, 1, "title",
                           pos=(50,(screen_height*2)/3),
@@ -17,37 +17,25 @@ class TextDisplay(wx.Frame):
                           style=wx.NO_BORDER|
                           wx.FRAME_NO_TASKBAR | wx.STAY_ON_TOP)
         self.panel = wx.Panel(self, size=self.GetSize())
-        self.text = wx.StaticText(self.panel, -1, start_up_msg,
+        self.text = wx.StaticText(self.panel, -1, "",
                                   style=wx.ALIGN_CENTRE)
         font = wx.Font(30, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
                        wx.FONTWEIGHT_NORMAL)
         self.text.SetFont(font)
-        self.countdown = -1
-        self.text_timer = wx.Timer(self)
-        current_text = ""
-        self.Bind(wx.EVT_TIMER, self.ChangeText, self.text_timer)
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.TimingOver, self.timer)
+        self.DisplayMessage(start_up_msg, time)
 
-    def DisplayMessage(self, msg, countdown=-1, timer=-1):
-        self.text_timer.Stop()
-        self.current_text = msg
-        if countdown == -1:
-            self.text.SetLabel(self.current_text)
-            self.text.Wrap(self.GetSize()[0])
-            self.countdown = -1
-            if not timer == -1:
-                self.text_timer.Start(timer)
-        else:
-            self.countdown = countdown
-            self.text_timer.Start(1000)
-        
-    def ChangeText(self, event):
-        if self.countdown == -1:
-            self.text_timer.Stop()
-            pub.sendMessage("InitMsg", msg="text_display_finished")
-        else:
-            self.text.SetLabel(self.current_text + str(self.countdown))
-            self.text.Wrap(self.GetSize()[0])
-            self.countdown -= 1      
+    def TimingOver(self, event):
+        self.timer.Stop()
+        pub.sendMessage("InitMsg", msg="text display done")
+
+    def DisplayMessage(self, msg, time=0):
+        self.text.SetLabel(msg)
+        self.text.Wrap(self.GetSize()[0])
+        if time > 0:
+            self.timer.Start(time*1000)
+           
 
     def CloseWindow(self):
         self.Close()
@@ -63,7 +51,7 @@ if __name__ == "__main__":
             "Please try to stay still, the process will start in ")
             self.frame = TextDisplay()
             self.frame.Show(True)
-            self.frame.DisplayMessage(introduction_text, 20)
+            self.frame.DisplayMessage(introduction_text)
             self.Bind(wx.EVT_CHAR_HOOK, self.KeyPress)
             return True
 
