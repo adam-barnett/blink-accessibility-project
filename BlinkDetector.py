673abc2cb45300956228a9f756fee33e2831f622
adam-barnett <adam.barnett23@gmail.com>
2015-03-21 15:53:33 -0400

    Fully added blink detection and messaging to BlinkDetector and removed all of the now unused functions

--- BlinkDetector.py
+++ BlinkDetector.py
@@ -37,6 +37,7 @@ class BlinkDetector():
             self.match_threshold = 0.7
             self.screen_width = screen_width
             self.screen_height = screen_height
+            self.eyes_open_msg_sent = True
             
             video_src = 0
             self.cam = cv2.VideoCapture(video_src)
@@ -96,23 +97,23 @@ class BlinkDetector():
                 self.SetEyeRegions()
                 cur_img = rotate(gray, self.rotation)
                 if(self.CheckForBlink(cur_img)):
-                    winsound.Beep(2500, 50)
-                
-                
-                #set_eye_areas
-                #check for eye matches
-                #deal with blink if found
-                #if no blink found then display 
-
-                cv2.rectangle(cur_img, (self.nose.l, self.nose.t),
-                              (self.nose.r, self.nose.b), (255,0,0), 2)
-                cv2.rectangle(cur_img, (self.left.l, self.left.t),
+                    cv2.rectangle(cur_img, (self.left.l, self.left.t),
                               (self.left.r, self.left.b), (0,0,0), 2)
-
-                #display_img = cv2.resize(cur_img, (0,0), fx=0.7, fy=0.7)
-                #xpos = self.screen_width - int(display_img.shape[0]*1.5)
-                #ypos = self.screen_height/3  - display_img.shape[1]/2
-                #cv2.imshow("full image", display_img)
+                    cv2.rectangle(cur_img, (self.right.l, self.right.t),
+                              (self.right.r, self.right.b), (0,0,0), 2)
+                    winsound.Beep(2500, 50)
+                    self.eyes_open_msg_sent = False
+                    pub.sendMessage("SwitchInput", msg="shut")
+                else:
+                    if not self.eyes_open_msg_sent:
+                        #inform that the eyes are open again
+                        self.eyes_open_msg_sent = True
+                        pub.sendMessage("SwitchInput", msg="open")
+                    if(self.left.l is not None):
+                        #then check for scale here
+                        pass
+                
+                
                 xpos = self.screen_width - int(cur_img.shape[0]*1.5)
                 ypos = self.screen_height/3  - cur_img.shape[1]/2
                 cv2.imshow("full image", cur_img)
@@ -192,104 +193,23 @@ class BlinkDetector():
              r_open_val > self.match_threshold):
                 #open eyes were found
                 left_area = self.left.GetArea()
-                self.left.SetAreaOffset(l_open_loc, self.left.norm.shape,
+                self.left.SetAreaOffset(l_open_loc,
+                                        self.left.norm.shape,
                                         left_area)
                 right_area = self.right.GetArea()
-                self.right.SetAreaOffset(r_open_loc, self.right.norm.shape,
+                self.right.SetAreaOffset(r_open_loc,
+                                         self.right.norm.shape,
                                          right_area)
-                                        
         else:
             #no good match was found
             self.left.SetArea(None)
             self.right.SetArea(None)
-        return False
-        
+        return False     
 
     def Match(self, big_img, small_img):
         matches = cv2.matchTemplate(big_img, small_img, cv2.TM_CCOEFF_NORMED)
         _, val, _, loc = cv2.minMaxLoc(matches)
         return val, loc
-            
-        
-
-
-    #not currently used
-    def SearchImageFromRect(self, img, gray, rects):
-        big_width = 0
-        big_height = 0
-        search_image = None
-        pos = None
-        for x, y, width, height in rects:
-            if(width > big_width and height > big_height):
-                big_width = width
-                big_height = height
-                pos = (x,y)
-        if big_width != 0 and big_height != 0:
-            if(big_width < self.shut_shape[1] or
-               big_width < self.open_shape[1]):
-                  big_width = max(self.shut_shape[1], self.open_shape[1])
-            if(big_height < self.shut_shape[0] or
-               big_height < self.open_shape[0]):
-                  big_height = max(self.shut_shape[0], self.open_shape[0])
-            cv2.rectangle(img, (pos[0], pos[1]),
-                          (pos[0]+big_width, pos[1]+big_height), (0,255,0), 2)
-            search_image = gray[pos[1]:pos[1]+big_height,
-                                pos[0]:pos[0]+big_width]
-        return (search_image, pos)
-
-    #not currently used
-    def EyesFromFace(self, face_rect):
-        [x, y, width, height] = face_rect
-        eyes_top = y + int(height * 0.294)
-        eyes_bottom = int(math.ceil(height * 0.221))
-        return [[x - 10, eyes_top - 10, width + 10, eyes_bottom + 10]]
-
-    #not currently used
-##    def CheckForBlink(self, search_image):
-##        method = eval(self.COMP_METHOD)
-##        shut_res = cv2.matchTemplate(search_image,self.shut_eyes,method)
-##        min_shut_val, max_shut_val, min_shut_loc, max_shut_loc = \
-##              cv2.minMaxLoc(shut_res)
-##        open_res = cv2.matchTemplate(search_image,self.open_eyes,method)
-##        min_open_val, max_open_val, min_open_loc, max_open_loc = \
-##              cv2.minMaxLoc(open_res)
-##        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-##            if min_shut_val < min_open_val:
-##                blink_pos = min_shut_loc
-##            else:
-##                blink_pos = None
-##        else:
-##            if self.test:
-##                self.close_vals.append(max_shut_val)
-##                self.open_vals.append(max_open_val)
-##            if(max_shut_val - self.blink_correction > max_open_val and
-##               max_shut_val > self.blink_threshold):
-##                blink_pos = max_shut_loc
-##            else:
-##                blink_pos = None
-##        return blink_pos
-
-    #not currently used
-    def BlinkFound(self, pos, img, blink_pos):
-        self.eyes_open_msg_sent = False
-        if pos is not None:
-            blink_pos = (blink_pos[0] + pos[0], blink_pos[1] + pos[1])
-        blink_img = img[blink_pos[1]:blink_pos[1]+self.shut_shape[0],
-                        blink_pos[0]:blink_pos[0]+self.shut_shape[1]]
-        blink_bottom_right = (blink_pos[0] + self.shut_shape[1],
-                              blink_pos[1] + self.shut_shape[0])
-        cv2.rectangle(img, blink_pos, blink_bottom_right, (255,0,0), 2)
-        #EVENTUALLY - I want to save the blink_img here if eyes were
-        #successfully found, for use in subsequent iterations (to account
-        #for small changes in light over time)
-        pub.sendMessage("SwitchInput", msg="shut")
-
-    #not currently used
-    def EyesOpenMesssage(self):
-        if not self.eyes_open_msg_sent:
-            #inform that the eyes are open again
-            self.eyes_open_msg_sent = True
-            pub.sendMessage("SwitchInput", msg="open")
 
     def Close(self):
         cv2.destroyAllWindows()
